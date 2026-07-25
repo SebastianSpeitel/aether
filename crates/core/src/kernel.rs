8ce04470f2868ece8923cd19afa20fdade585453
Sebastian Speitel <sebastian.speitel@fem.tu-ilmenau.de>
2026-07-25 12:12:07 +0200

    core: add default wake method to Kernel trait

--- crates/core/src/kernel.rs
+++ crates/core/src/kernel.rs
@@ -1,14 +1,24 @@
-use aether_time as time;
 use core::error::Error;
+
+use aether_time as time;
 
 /// Trait representing an execution context passed to tasks during polling.
 pub trait Kernel {
-    /// Registers that the task wants to be polled again
-    fn yield_for<C: time::Clock>(&mut self, dur: time::Duration<C>);
-
     #[inline]
-    fn r#yield(&mut self) {
+    fn r#yield(&self) {
         self.yield_for(time::Duration::<time::FrozenClock>::ZERO);
+    }
+
+    /// Registers that the task wants to be polled again
+    fn yield_for<C: time::Clock>(&self, dur: time::Duration<C>);
+
+    /// Notifies the kernel that an asynchronous event has occurred and tasks should be polled.
+    ///
+    /// Unlike `r#yield` (which is called during polling to yield CPU time), `wake` is typically
+    /// called out-of-band by event producers or wakers.
+    #[inline]
+    fn wake(&self) {
+        self.r#yield();
     }
 }
 
